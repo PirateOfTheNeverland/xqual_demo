452ccd60d7f64c0993a5a18b411e8c4b6edbec7e
Andrey Z <profforg@inbox.ru>
2024-10-07 21:37:54 +0300

    Test coverage for June

---
+++ overall_mood.py
@@ -0,0 +1,6 @@
+def test_overall_mood():
+    assert 1 == 0, "Should be AWESOME"
+
+if __name__ == "__main__":
+    test_overall_mood()
+    print("Everything passed")
---
+++ temperature/temperature_in_june.py
@@ -0,0 +1,6 @@
+def test_temperature_in_june():
+    assert 20 == 20, "Should be 20"
+
+if __name__ == "__main__":
+    test_temperature_in_june()
+    print("Everything passed")
